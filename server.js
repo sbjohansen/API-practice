5faac68ba7ff797a84be52b38e82730cee541a95
Slawomir Johansen <baran@fastmail.com>
2022-08-02 19:17:30 +0200

    TEST heroku change

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('*', (req, res) => {
 });
 
 app.use((req, res) => {
-  res.status(404).send('404 not found...');
+  res.status(404).send('404 page not found...');
 });
 
 const server = app.listen(process.env.PORT || 8000, () => {
